d41750fb0bf575df460c1dce354f8e8a27e98349
karthik-s-40 <130835550+karthik-s-40@users.noreply.github.com>
2025-02-19 09:33:16 +0530

    Update sample.py

--- cron/sample.py
+++ cron/sample.py
@@ -96,7 +96,7 @@ try:
     # Auto-commit the updated model
     os.system("git config --global user.email 'github-actions@github.com'")
     os.system("git config --global user.name 'GitHub Actions'")
-    os.system("git add cron/air_quality_model.pkl")
+    os.system("git add Backend/air_quality_model.pkl")
     os.system("git commit -m 'Update air quality model'")
     os.system("git push origin main")  # Change "main" if using another branch
 
